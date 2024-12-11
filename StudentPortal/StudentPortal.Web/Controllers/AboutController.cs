107c9a6f8274ada5141eacbc09448f486ed4e617
Varshil Bhojak <Varshil.Bhojak@injala.com>
2024-12-11 23:13:40 +0530

    first commit

---
+++ StudentPortal/StudentPortal.Web/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
